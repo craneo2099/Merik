13cdc15579bcb55ff56ded928dedce2ef7b6de76
craneo2099 <crankras@hotmail.com>
2021-07-09 17:08:12 -0500

    buscador arreglado

--- src/template/end.php
+++ src/template/end.php
@@ -2,15 +2,3 @@
 	<script src="js/popper.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 	<script src="js/funciones.js"></script>
-	<script>
-	  (function() {
-	    var cx = '012137303303141424084:dqyvcbjtmok';
-	    var gcse = document.createElement('script');
-	    gcse.type = 'text/javascript';
-	    gcse.async = true;
-	    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-	    var s = document.getElementsByTagName('script')[0];
-	    s.parentNode.insertBefore(gcse, s);
-	  })();
-	</script>
-	<gcse:searchbox></gcse:searchbox>
